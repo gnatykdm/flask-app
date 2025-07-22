57db5a53ce596bbdc7dfb9b3a8c231a6f800ec0e
gnatykdm <gnatykwork@gmail.com>
2025-07-22 13:36:26 +0200

    Update[main]

--- app/main.py
+++ app/main.py
@@ -10,11 +10,12 @@ from .service.dto import *
 logger: Logger = get_logger()
 app: Flask = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "https://wohlfahrt.com.ua"}}, supports_credentials=True)
+# Разрешаем CORS для всех источников
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'https://wohlfahrt.com.ua'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     return response
